9310aca59e12d684a3d646f8e49e0ae8a6452588
Daniil <dumchenko.de@sibvaleo.com>
2021-11-24 09:51:50 +0700

    generate csv, need nueral network model

--- init.py
+++ init.py
@@ -1,31 +1,43 @@
 import aiohttp
 import asyncio
 import pprint
+import aiofiles
+import csv
 
 from yarl import URL
 
 class GetPrice():
 
-    def __init__(self, name, price, date, url) -> None:
+    def __init__(self, name, url) -> None:
         self.name = name
-        self.price = price
-        self.date = date
+        # self.price = price
+        # self.date = date
         self.url = url
 
 
 
-    async def get_data_for_url(url):
+    async def get_data_for_url(self):
         timeout = aiohttp.ClientTimeout(total=120)
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resposne:
-                print(await resposne.text())
+            async with session.get(self.url) as resposne:
+                data = await resposne.json()
+                return data.get('prices')
+
+    def write_csv(self, data):
+        with open('test.csv', mode="w", newline='') as file:
+            split_data = data[0]
+            writer = csv.writer(file)
+            writer.writerows(zip(split_data.split(', ')))           
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1') as resposne:
-            k = await resposne.json()
-            pprint.pprint(k.get('prices'))
+    get = GetPrice(name='Bitcoin', url='https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1')
+    data_to_csv = await get.get_data_for_url()
+    
+
+    res = ["".join([", ".join([str(i) for i in s]) for s in data_to_csv])]
+    get.write_csv(res)
+    
 
 
 loop = asyncio.get_event_loop()
